463963b3b7150c68cce02519d6bbd183f0916d8e
abdibrokhim <abdibrokhim@gmail.com>
2023-07-24 20:20:18 +0500

    Initial Commit

---
+++ crunchitWithAuth/crunchitWithAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  crunchitWithAuthApp.swift
+//  crunchitWithAuth
+//
+//  Created by Ibrohim Abdivokhidov on 24/07/23.
+//
+
+import SwiftUI
+
+@main
+struct crunchitWithAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
